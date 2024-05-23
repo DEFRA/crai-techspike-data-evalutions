056086dfdef1ae65c02a45dedd077a67b477f1a6
alistairknight <alistair@nts-graphics.co.uk>
2024-05-23 16:59:02 +0100

    Ollama / Llama3 update

--- app/llm/ai.js
+++ app/llm/ai.js
@@ -1,5 +1,16 @@
 const { ChatOpenAI, OpenAIEmbeddings } = require('@langchain/openai')
+const { ChatOllama } = require('@langchain/community/chat_models/ollama')
+const { OllamaEmbeddings } = require('@langchain/community/embeddings/ollama')
+
 const aiConfig = require('../config/ai')
+
+const useModel = 'ollama'
+const ollamaModel = 'llama3'
+/*
+$ ollama pull llama3
+$ ollama run llama3
+*/
+
 
 const onFailedAttempt = async (error) => {
   if (error.retriesLeft === 0) {
@@ -8,16 +19,35 @@ const onFailedAttempt = async (error) => {
 }
 
 const embeddings = () => {
+  if (useModel === 'ollama') {
+    return new OllamaEmbeddings({
+      baseUrl: 'http://host.docker.internal:11434',
+      model: ollamaModel,
+      requestOptions: {
+        useMMap: true,
+        numThread: 6,
+        numGpu: 1
+      }
+    })
+  }
+
   return new OpenAIEmbeddings({
     ...aiConfig,
-    azureOpenAIApiDeploymentName: "ada-002"
+    azureOpenAIApiDeploymentName: 'ada-002'
   })
 }
 
 const model = (temperature = 0.9) => {
+  if (useModel === 'ollama') {
+    return new ChatOllama({
+      baseUrl: 'http://host.docker.internal:11434',
+      model: ollamaModel
+    })
+  }
+
   return new ChatOpenAI({
     ...aiConfig,
-    azureOpenAIApiDeploymentName: "gpt-53-turbo",
+    azureOpenAIApiDeploymentName: 'gpt-53-turbo',
     onFailedAttempt,
     temperature: temperature
   })
