7b1254ff49bbbb7750fcc64675734055c0f174f8
barrygeedefra <barry.gee@defra.gov.uk>
2024-05-23 11:39:06 +0100

    Refactor document ingestion route and update AI dependencies

--- app/routes/documents.js
+++ app/routes/documents.js
@@ -19,27 +19,15 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       let response;
-      
+
       try {
         const llm = model()
         const prompt = prompts[types.GENERATE_PROMPT]
         response = await generateResponse(llm, prompt, 'What is NEIRF?')
-      
-      
-        //const response = await run('Translate "Hello World" into German.')
-        console.log(response)
       }
       catch(error) {
         console.log(error)
       }
-      
-      // const docs = await loadFile('./data/NEIRF/15458_NEIRF_YR1_REPORT_FINAL.PDF')
-      
-      // console.log(docs)
-      
-      // //const docs = loadFilesFromFolder('./data/NEIRF')
-  
-      // await ingestDocuments(docs)
       
       return { response }
     }
